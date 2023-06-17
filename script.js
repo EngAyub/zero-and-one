9fb789ae276fea70f0fe3983ec42a88d10d3b3a1
ENG AYUUB <moztaphem9@gmal.com>
2023-06-17 20:38:30 +0300

    zero and one Done

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 // Good Luck 💪🏾
+
+
+function ZeroAndOne(AK) {
+    let result = '';
+    for (let i = 0; i < AK.length; i++) {
+      if (AK[i] === 'Zero') {
+        result += '0';
+      } else if (AK[i] === 'One') {
+        result += '1';
+      }
+    }
+    return result;
+  }
+  console.log(ZeroAndOne(["one","Zero","Zero","One"])); 
+  console.log(ZeroAndOne(["Zero", "ONE", "one", "Zero"])); 
+ 
